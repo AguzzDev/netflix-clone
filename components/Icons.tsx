4aa53a5113c382838df3163b7691497eb1e05484
AguzZDev <example@example.com>
2022-02-18 23:26:18 -0300

    changing types on icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,7 +1,5 @@
-import { SVGProps } from "react"
-
 interface Props {
-  Icon: SVGProps<SVGSVGElement>
+  Icon: React.ElementType<React.ComponentPropsWithRef<"svg">>
   color: string
 }
 
@@ -9,8 +7,6 @@ export const IconsXXS = ({ Icon, color }: Props): JSX.Element => {
   return <Icon className={`w-5 h-5 text-${color}`} />
 }
 
-
 export const IconsXS = ({ Icon, color }: Props): JSX.Element => {
   return <Icon className={`w-6 h-6 text-${color}`} />
 }
-
