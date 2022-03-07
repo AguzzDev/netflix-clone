59226cdf67290b513d2bc63e82871183689bbb17
AguzZDev <example@example.com>
2022-03-07 00:50:39 -0300

    yes now ...

--- graphql/provider.tsx
+++ graphql/provider.tsx
@@ -1,30 +1,30 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import { RestLink } from "apollo-link-rest"
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    getSearch: {
-      fields: {
-        search: {
-          keyArgs: [],
-          merge(existing, incoming, { args: { page = 1 } }) {
-            const merged = existing ? existing.slice(0) : []
-            for (let i = 0; i < incoming.length; ++i) {
-              merged[page + i] = incoming[i]
-            }
-            return merged
-          },
-        },
-      },
-    },
-  },
-})
+// const cache = new InMemoryCache({
+//   typePolicies: {
+//     getSearch: {
+//       fields: {
+//         search: {
+//           keyArgs: [],
+//           merge(existing, incoming, { args: { page = 1 } }) {
+//             const merged = existing ? existing.slice(0) : []
+//             for (let i = 0; i < incoming.length; ++i) {
+//               merged[page + i] = incoming[i]
+//             }
+//             return merged
+//           },
+//         },
+//       },
+//     },
+//   },
+// })
 
 function Provider({ children }: any) {
   const restLink = new RestLink({ uri: `https://api.themoviedb.org/3/` })
   const client = new ApolloClient({
     link: restLink,
-    cache,
+    cache: new InMemoryCache(),
   })
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>
